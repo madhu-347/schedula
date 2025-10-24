6a8a73f8702bcff4b2cf5c88a6445fb2e3dac195
Bharath18sv <svbharath2005@gmail.com>
2025-10-24 12:18:05 +0530

    Resolve merge conflicts, Update doctor id page and appointment review page, Fix dashboard UI for responsiveness

--- schedula/src/app/user/(auth)/login/page.tsx
+++ schedula/src/app/user/(auth)/login/page.tsx
@@ -34,7 +34,7 @@ export default function LoginPage() {
     console.log("🔐 Checking mock user credentials...");
 
     try {
-      const user = mockData.doctors.find(
+      const user = mockData.users.find(
         (u) => u.email === formData.email || u.phone === formData.email
       );
 
---
+++ schedula/src/app/user/appointment/[doctorId]/page.tsx
@@ -0,0 +1,16 @@
+// this page should match Booking 6 page in the figma designs
+
+import { Button } from '@/components/ui/Button'
+import Link from 'next/dist/client/link'
+import React from 'react'
+import page from '../review/page'
+
+function AppointmentBook() {
+  return (
+    <div>
+      Appointment Scheduling page
+    </div>
+  )
+}
+
+export default AppointmentBook
